fbca7847c12e38fc7c9aa66044e910d59b845c29
Roger Graham <roger.graham@tuni.fi>
2022-04-26 20:52:46 +0300

    Update index.js: Import BowserRouter, Routes & Route depencies.  Add Route paths.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import AdminForm from "./routes/admin";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/admin" element={<AdminForm />} />
+          {/* <Route path="completed" element={<Completed />} />
+          <Route path="incomplete" element={<Incomplete />} />
+          <Route path="app-info" element={<Info />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    , rootElement );
   </React.StrictMode>
 );
 
