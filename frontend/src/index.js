e88dc56434ee49cfb81567904e6a793b4b6ca14a
Roger Graham <roger.graham@tuni.fi>
2022-05-02 16:44:17 +0300

    Update index.js: add basename attribute to BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/" element={<Home />} />
