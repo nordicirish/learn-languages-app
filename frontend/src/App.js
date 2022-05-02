a32624ca408776ba432250c7095e1658e13eb780
Roger Graham <roger.graham@tuni.fi>
2022-05-02 16:43:11 +0300

    Update App.js : Change to using NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,20 @@
 import "./App.css";
-import { Outlet, Link } from "react-router-dom";
+// import { Outlet, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-const App = () => {
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
   return (
-    <div>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          padding: "1rem",
-          backgroundColor: "#03a9f4",
-          fontSize: "2rem",
-          textAlign: "center",
-          paddingBottom: "1rem",
-          marginBottom: "3rem",
-          fontFamily: "Tahoma, Verdana, Segoe, sans-serif",
-        }}
-      >
-        <Link to="/">Home</Link> | <Link to="/admin">Admin</Link>
-      </nav>
-      <Outlet />
+    <div className="container">
+      <div className="box">
+        <NavBar />
+        <Outlet />
+      </div>
     </div>
   );
-};
-
+}
 export default App;
