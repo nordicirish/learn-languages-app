eb7ae36fe061ddd59f858e9c6e2d65e47ab05461
Roger Graham <roger.graham@tuni.fi>
2022-05-04 19:20:50 +0300

    Update NavBar.js : Add  bootsrap responsive behaviour to menu

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,20 +11,25 @@ import Container from "react-bootstrap/esm/Container";
 const NavBar = () => {
   return (
     <>
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="/">
-          <h1>Learn Languages App</h1>
-        </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" href="/admin">
-              Admin
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <h1>Learn Languages App</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Item>
+                <Nav.Link href="/">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1" href="/translations">
+                  Admin
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
