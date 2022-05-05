0e33fd85e1ad92c23153e15cfc33136ab02aea59
Roger Graham <roger.graham@tuni.fi>
2022-05-05 19:18:36 +0300

    Add edit.js; Edit form with update and cancel functions

---
+++ frontend/src/routes/edit.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+const EditTranslation = () => {
+  //useNavigate hook allows to navigate away from the current url
+  let navigate = useNavigate();
+  // useParam hook captures url paramater
+  const { id } = useParams();
+
+  const [translation, setTranslation] = useState({
+    english: "",
+    finnish: "",
+    tag_id: "",
+  });
+
+  const { english, finnish, tag_id } = translation;
+
+  const onInputChange = (e) => {
+    setTranslation({ ...translation, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    getTranslation();
+  }, []);
+
+  const updateTranslation = async (e) => {
+    e.preventDefault();
+    await axios.put(
+      `http://localhost:8080/translations/update/${id}`,
+      translation
+    );
+
+    navigate("/translations");
+  };
+
+  const cancelUpdate = async (e) => {
+    // prevents buton acting as a submit button
+    e.preventDefault();
+    navigate("/translations");
+  };
+
+  const getTranslation = () => {
+    axios
+      .get(`http://localhost:8080/translations/find/${id}`)
+      .then((response) => {
+        console.log("promise fulfilled");
+        setTranslation({
+          id: id,
+          english: response.data.english,
+          finnish: response.data.finnish,
+          tag_id: response.data.tag_id,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <Container>
+      <div className="row mt-4">
+        <div className="col-sm-5 col-offset-3 mx-auto shadow p-5">
+          <h4 className="text-center mb-4">Edit a translation</h4>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Control
+                as="input"
+                type="text"
+                name="english"
+                value={english}
+                onChange={(e) => onInputChange(e)}
+                placeholder="Enter the English word"
+                required
+                pattern="[a-zA-Z]*"
+                title="The word should have only English letters"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Control
+                as="input"
+                type="text"
+                name="finnish"
+                value={finnish}
+                onChange={(e) => onInputChange(e)}
+                placeholder="Enter the Finnish translation"
+                pattern="[a-zA-ZäöåÄÖÅ]*"
+                title="The word should have only letters"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="text"
+                name="tag"
+                value={tag_id}
+                onChange={(e) => onInputChange(e)}
+                placeholder="Enter the tag"
+                required
+                pattern="[a-zA-Z]*"
+                title="The word should have only English letters"
+              />
+            </Form.Group>
+
+            <Button type="submit" id="1" onClick={updateTranslation}>
+              Update
+            </Button>
+            <Button
+              variant="secondary"
+              type="button"
+              id="2"
+              onClick={cancelUpdate}
+            >
+              Cancel
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default EditTranslation;
