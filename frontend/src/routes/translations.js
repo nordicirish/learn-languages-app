c184ece794dc15fe8d7ebce2b753ca88e87abf56
Roger Graham <roger.graham@tuni.fi>
2022-05-08 16:49:38 +0300

    Update translations.js: Move submit form to it's own component
    Object destructuring and onInputChange event handler moved to SubmitForm component

--- frontend/src/routes/translations.js
+++ frontend/src/routes/translations.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import Toast from "react-bootstrap/Toast";
 import Container from "react-bootstrap/Container";
-
 import AdminTable from "../components/AdminTable";
+import SubmitForm from "../components/SubmitForm";
 
 // "proxy": "http://localhost:8080" in package.json allows short urls
 
@@ -22,12 +20,6 @@ const Translations = () => {
   });
 
   const [tags, setTags] = useState([]);
-
-  //  Object Destructuring
-  const { english, finnish, tag_id } = translation;
-  const onInputChange = (e) => {
-    setTranslation({ ...translation, [e.target.id]: e.target.value });
-  };
 
   // On Page load display all records
   const getTranslations = () => {
@@ -104,107 +96,16 @@ const Translations = () => {
       </Toast>
       <Container>
         <div className="row d-flex flex-row-reverse mt-3">
-          <div className="col-sm-4">
-            <div
-              className="box p-3 mb-3 mt-0"
-              style={{ border: "1px solid #d0d0d0" }}
-            >
-              <Form onSubmit={submitTranslation}>
-                <h4 className="mb-3 ">Add a translation</h4>
-                <Form.Group className="mb-3">
-                  <Form.Control
-                    as="input"
-                    type="text"
-                    id="english"
-                    name="english"
-                    value={english}
-                    onChange={(e) => onInputChange(e)}
-                    placeholder="Enter the English word"
-                    aria-label="Enter the Finnish translation"
-                    required
-                    pattern="[a-zA-Z]*"
-                    title="The word should have only English letters"
-                  />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                  <Form.Control
-                    as="input"
-                    type="text"
-                    id="finnish"
-                    name="finnish"
-                    value={finnish}
-                    onChange={(e) => onInputChange(e)}
-                    placeholder="Enter the Finnish translation"
-                    aria-label="Enter the Finnish translation"
-                    pattern="[a-zA-ZäöåÄÖÅ]*"
-                    title="The word should have only letters"
-                    required
-                  />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                  <Form.Select
-                    id="tag_id"
-                    value={tag_id}
-                    onChange={(e) => onInputChange(e)}
-                    aria-label="Please select a tag"
-                    required
-                  >
-                    <option value="">Select a tag</option>
-                    {tags.map((tag) => (
-                      <option key={tag.tag} value={tag.tag}>
-                        {tag.tag}
-                      </option>
-                    ))}
-                  </Form.Select>
-                </Form.Group>
-                <Button type="submit">Add</Button>
-              </Form>
-            </div>
-          </div>
+          <SubmitForm
+            tags={tags}
+            translation={translation}
+            submitTranslation={submitTranslation}
+            setTranslation={setTranslation}
+          />
           <AdminTable
             translations={translations}
             deleteTranslation={deleteTranslation}
           />
-          {/* <div className="col-sm-8">
-            <Table
-              borderless
-              size="m"
-              className="table admin-table table-striped   "
-            >
-              <thead className="thead-dark thead-blue">
-                <tr>
-                  <th>English</th>
-                  <th>Finnish</th>
-                  <th style={{ width: "22%" }}>Tag</th>
-                  <th style={{ width: "8%" }}>Edit</th>
-                </tr>
-              </thead>
-              <tbody>
-                {translations.map((translation) => (
-                  <tr key={translation.id}>
-                    <td>{translation.english}</td>
-                    <td>{translation.finnish}</td>
-                    <td>{translation.tag_id}</td>
-                    <td>
-                      <DeleteModal
-                        translation={translation}
-                        deleteTranslation={deleteTranslation}
-                      />
-
-                      <Link
-                        className=" mr-2"
-                        to={`/translations/edit/${translation.id}`}
-                      >
-                        <i className="fa fa-edit" aria-hidden="true"></i>
-                      </Link>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div> */}
         </div>
       </Container>
     </section>
