d53de3ce77868c3e626ede2b8979b7224b184844
Roger Graham <roger.graham@tuni.fi>
2022-05-09 16:25:27 +0300

    Update home.js: 1st basic version of the quiz
    Uses a form a UI

--- frontend/src/routes/home.js
+++ frontend/src/routes/home.js
@@ -1,70 +1,92 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import AdminTable from "../components/AdminTable";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const Home = () => {
   // useState function to initialize the piece of state stored in translations with the array of translataion values passed in the props:
-  // const [translations, setTranslations] = useState([]);
-  // const [newEnglish, setNewEnglish] = useState("");
-  // const [newFinnish, setNewFinnish] = useState("");
-  // const [newTag, setNewTag] = useState("");
+  const [translations, setTranslations] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [userAnswer, setUserAnswer] = useState("");
+  const getTranslations = () => {
+    axios
+      .get("/api/all")
+      .then((response) => {
+        console.log("promise fulfilled");
+        setTranslations(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  useEffect(() => {
+    getTranslations();
+  }, []);
 
-  // //useEffect hooks fetches data using axios
-  // useEffect(() => {
-  //   console.log("effect");
-  //   axios.get("http://localhost:8080/translations").then((response) => {
-  //     console.log("promise fulfilled");
-  //     //initialise the data in the translations array
-  //     setTranslations(response.data);
-  //   });
-  // }, []);
-  // console.log("render", translations.length, "translations");
-
-  // // event handlers
-
-  // const addTranslation = (event) => {
-  //   event.preventDefault();
-  //   //stops page reload and other unwanted default behaviour
-  //   const translationObject = {
-  //     english: newEnglish,
-  //     finnish: newFinnish,
-  //     tag_id: newTag,
-  //   };
-  //   axios
-  //     .post("http://localhost:8080/translations/add", translationObject, {
-  //       headers: {
-  //         "content-type": "application/json",
-  //       },
-  //     })
-  //     .then((response) => {
-  //       console.log(response);
-  //       setTranslations(translations.concat(response.data));
-  //       setNewEnglish("");
-  //       setNewFinnish("");
-  //       setNewTag("");
-  //     });
-  // };
-
-  // const handleEnglishChange = (event) => {
-  //   console.log(event.target.value);
-  //   setNewEnglish(event.target.value);
-  // };
-
-  // const handleFinnishChange = (event) => {
-  //   console.log(event.target.value);
-  //   setNewFinnish(event.target.value);
-  // };
-
-  // const handleTagChange = (event) => {
-  //   console.log(event.target.value);
-  //   setNewTag(event.target.value);
-  // };
+  const submitAnswer = async (e) => {
+    e.preventDefault();
+    alert("correct answer " + correctAnswer);
+    alert("submitted answer " + userAnswer);
+  };
+  // const finnish = translation.finnish;
+  const onInputChange = (e) => {
+    setCorrectAnswer(e.target.name);
+    setUserAnswer(e.target.value);
+  };
 
   return (
-    <div className="container">
-      <div className="box">
-        <h1>Learn Languages App</h1>
-        <h2>Coming soon...</h2>
+    <div className="row mt-4">
+      <div className="col-sm-8 col-offset-3 mx-auto shadow p-5">
+        <h4 className="text-center mb-4">Test yourself</h4>
+        <Form>
+          {translations.map((translation) => (
+            <Row key={translation.id} className="align-items-center">
+              <Col sm={5} className="my-1">
+                <InputGroup size="lg">
+                  <InputGroup.Text
+                    id="language1"
+                    aria-describedby="English word"
+                  >
+                    {translation.english}
+                  </InputGroup.Text>
+                  <Form.Control
+                    id={translation.id}
+                    name={translation.finnish}
+                    placeholder="Finnish word"
+                    onChange={(e) => onInputChange(e)}
+                    aria-label="Enter the Finnish translation"
+                    pattern="[a-zA-ZäöåÄÖÅ]*"
+                    title="The word should have only letters"
+                    required
+                  />
+                  <Form.Label
+                    id="correct"
+                    htmlFor="inlineFormInputGroupUsername"
+                    visuallyHidden
+                  >
+                    You got it right!
+                  </Form.Label>
+                  <Form.Label
+                    htmlFor="inlineFormInputGroupUsername"
+                    visuallyHidden
+                  >
+                    Oops wrong answer!
+                  </Form.Label>
+                </InputGroup>
+              </Col>
+
+              <Col xs="auto" className="my-1">
+                <Button type="submit" onClick={submitAnswer}>
+                  Answer
+                </Button>
+              </Col>
+            </Row>
+          ))}
+        </Form>
       </div>
     </div>
   );
