6378a301ccd44b8c21536f2f3ccadc30042c316d
Roger Graham <roger.graham@tuni.fi>
2022-05-12 18:01:38 +0300

    Update AdminTable.js: change bootstrap className and inline styles to improve responsiveness

--- frontend/src/components/AdminTable.js
+++ frontend/src/components/AdminTable.js
@@ -5,16 +5,18 @@ import { Link } from "react-router-dom";
 
 // pass translations as a named prop to ensure it is passed as an array
 // set an attribute translations = {translations} in component tag in app.js
-const AdminTable = ({ translations, deleteTranslation }) => {
+const AdminTable = ({ tags, translations, deleteTranslation }) => {
   return (
-    <div className="col-sm-8">
+    <div className="col-sm-12">
       <Table borderless size="m" className="table admin-table table-striped   ">
         <thead className="thead-dark thead-blue">
           <tr>
             <th>English</th>
             <th>Finnish</th>
-            <th style={{ width: "22%" }}>Tag</th>
-            <th style={{ width: "8%" }}>Edit</th>
+            <th style={{ width: "25%" }}>Tag</th>
+            <th style={{ width: "14%", minWidth: "14%", maxWidth: "14%" }}>
+              Edit
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -23,7 +25,7 @@ const AdminTable = ({ translations, deleteTranslation }) => {
               <td>{translation.english}</td>
               <td>{translation.finnish}</td>
               <td>{translation.tag_id}</td>
-              <td>
+              <td className="text-center">
                 <DeleteModal
                   translation={translation}
                   deleteTranslation={deleteTranslation}
