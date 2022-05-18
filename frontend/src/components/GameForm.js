dc008e6ab590e34ba7f586a5e00c93563ed1e4ab
Roger Graham <roger.graham@tuni.fi>
2022-05-18 18:10:50 +0300

    Update GameForm.js: Update functionaliity and appearance
    import id, setId(), setButtonId(), handleSubmit() setUserAnswer() setRightAnswer() props
    
    tranalstions .map() add index to mapped array
    Set Row key, Button key and Form Control to reference the index values
    
    Add a new column twith buttons yto take user input for a better experience
    
    A ternany opoerator switches between displaying a button on each row or a smilie icon depending on whether the user input is correct

--- frontend/src/components/GameForm.js
+++ frontend/src/components/GameForm.js
@@ -6,13 +6,19 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.min.css";
 import InputGroup from "react-bootstrap/InputGroup";
+import { ButtonGroup } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const GameForm = ({
+  id,
+  setId,
+  setButtonId,
   translations,
   isDisabled,
-  update,
-  handleSubmit,
   onInputChange,
+  handleSubmit,
+  setUserAnswer,
+  setRightAnswer,
 }) => {
   const [reset, setReset] = useState();
 
@@ -25,31 +31,33 @@ const GameForm = ({
 
   return (
     <Form onSubmit={handleSubmit}>
-      {translations.map((translation) => (
+      {translations.map((translation, index) => (
         <Row
-          key={translation.id}
+          key={index}
           // id={translation.id}
-          className="justify-content-center mt-2"
+          className="justify-content-center mt-3 align-items-center"
         >
-          <Col sm={12} m={12} lg={10} className="my-1">
-            <InputGroup size="lg">
+          <Col xs={10} sm={10} m={10} lg={10} className="my-1">
+            <InputGroup size="lg" key={index}>
               <InputGroup.Text
-                className="bg-info  bg-gradient fs-3 text justify-content-center"
+                className="bg-info bg-gradient fs-3 text justify-content-center"
                 aria-describedby="English word"
               >
                 {translation.english}
               </InputGroup.Text>
               <Form.Control
                 autoFocus={true}
+                spellCheck="false"
                 disabled={isDisabled}
                 className="fs-3 text-center"
-                id={translation.id}
+                //   add 1 or first entry will be 0 and the first input id won't be set a value
+                id={index + 1}
                 name={translation.finnish}
                 placeholder="Finnish word"
                 autoComplete="off"
-                // onChange={(e) => onInputChange(e)}
-                onChange={onInputChange}
-                // onKeyPress={handleKeypress}
+                onChange={(e) => {
+                  onInputChange(e);
+                }}
                 aria-label="Enter the Finnish translation"
                 pattern="[a-zA-ZäöåÄÖÅ]*"
                 title="The word should have only letters"
@@ -57,10 +65,40 @@ const GameForm = ({
               />
             </InputGroup>
           </Col>
+
+          <Col xs={2} className="justify-content-center">
+            {translation.isCorrect ? (
+              <span className="text-warning fs-3 text ">
+                <FontAwesomeIcon icon="fa-solid fa-face-smile" size="2x" />
+              </span>
+            ) : (
+              <span className="justify-content-center">
+                <Button
+                  key={index}
+                  onClick={handleSubmit}
+                  className="btn btn-success btn-lg"
+                >
+                  Go
+                  {/* <FontAwesomeIcon icon="fa-regular fa-tablet-button" /> */}
+                </Button>
+              </span>
+              //   <ButtonGroup className="row-button" >
+              //     <Button type="submit" >
+              //       Answer
+              //     </Button>
+              //   </ButtonGroup>
+            )}
+          </Col>
         </Row>
       ))}
-      <Row className="justify-content-center">
-        <Col xs={8} sm={8} m={8} lg={6} className="my-auto">
+      <Row className="justify-content-start">
+        <Col
+          xs={10}
+          sm={10}
+          m={10}
+          lg={10}
+          className="fs-3 text justify-content-center"
+        >
           <div className="d-grid gap-2">
             <Button
               className="mt-4 fs-2"
@@ -71,11 +109,6 @@ const GameForm = ({
               Reset
             </Button>
           </div>
-        </Col>
-        <Col xs="auto" className="my-1">
-          {/* <Button type="submit" onClick={handleSubmit} hidden>
-            Answer
-          </Button> */}
         </Col>
       </Row>
     </Form>
