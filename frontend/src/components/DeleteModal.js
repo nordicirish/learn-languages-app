32863d7f51cced4e12ac995e978edb153ec758a9
Roger Graham <roger.graham@tuni.fi>
2022-05-19 16:49:55 +0300

    Update DeleteModal.js: Close button retitled to Cancel Delete

--- frontend/src/components/DeleteModal.js
+++ frontend/src/components/DeleteModal.js
@@ -7,16 +7,17 @@ export default function DeleteModal({ translation, deleteTranslation }) {
   const [show, setShow] = useState(false);
   // functions handle state change
   const handleClose = () => setShow(false);
+  // modalbecomes visible
   const handleShow = () => setShow(true);
   return (
     <>
-      {" "}
       <a className="red-bin mr-2" onClick={handleShow}>
         <i
           className="far fa-trash-alt"
           style={{ fontSize: "18px", marginRight: "5px" }}
         ></i>
       </a>
+      {/* use show state to show or hide the modal */}
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>Deletion</Modal.Title>
@@ -24,11 +25,13 @@ export default function DeleteModal({ translation, deleteTranslation }) {
         <Modal.Body>Are you sure that you want to delete this item?</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            Cancel delete
           </Button>
           <Button
             variant="primary"
             onClick={async (e) => {
+              // calls the delete function passed as a prop from Translations.js
+              //  via AdminTable.js to the modal
               await deleteTranslation(translation.id);
               //close the modal to prevent a loop
               handleClose();
